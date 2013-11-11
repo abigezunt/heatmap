07c91b22548b2a54fd94a383b8338689b915d75e
Abby Howell <abby.g.howell@gmail.com>
2013-11-11 17:14:42 -0500

    added Facility class and linked to api_lab database

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 gem 'pry'
 gem 'sinatra'
 gem 'sinatra-reloader'
+gem 'pg'
+gem 'sinatra-activerecord’
--- main.rb
+++ main.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 require 'pry'
+
+
+set :database, {adapter: 'postgresql',
+                database: 'api_lab_development',
+                host: 'localhost'}
+
+class Facility < ActiveRecord::Base
+end
 
 get '/' do
   redirect_to '/index.html'	
 end
+
+
+get '/facilities.json' do
+  Facility.all.to_json
+end  
